2953fe20d5386b817c8ddde837ba7eefc3320d86
Michael Bowman <bowmanmc@gmail.com>
2015-07-21 19:40:55 -0400

    katakana level 3

---
+++ app/modules/levels/katakana/katakana03.js
@@ -0,0 +1,55 @@
+angular.module('kk').factory('LvlK03', function() {
+
+    return {
+        // Level 3 - Start
+        "id": "k03",
+        "name": "Level 3",
+        "description": "sa-so, za-zo",
+        "level": "3",
+        "type": "Katakana",
+        "characters": [{
+            "en": "sa",
+            "jp": "サ",
+            "sound": "lvl-03-char-sa.wav"
+        }, {
+            "en": "shi",
+            "jp": "シ",
+            "sound": "lvl-03-char-shi.wav"
+        }, {
+            "en": "su",
+            "jp": "ス",
+            "sound": "lvl-03-char-su.wav"
+        }, {
+            "en": "se",
+            "jp": "セ",
+            "sound": "lvl-03-char-se.wav"
+        }, {
+            "en": "so",
+            "jp": "ソ",
+            "sound": "lvl-03-char-so.wav"
+        }, {
+            "en": "za",
+            "jp": "ザ",
+            "sound": "lvl-03-char-za.wav"
+        }, {
+            "en": "ji",
+            "jp": "ジ",
+            "sound": "lvl-03-char-ji.wav"
+        }, {
+            "en": "zu",
+            "jp": "ズ",
+            "sound": "lvl-03-char-zu.wav"
+        }, {
+            "en": "ze",
+            "jp": "ゼ",
+            "sound": "lvl-03-char-ze.wav"
+        }, {
+            "en": "zo",
+            "jp": "ゾ",
+            "sound": "lvl-03-char-zo.wav"
+        }], // characters - level
+        "words": [{
+        }]
+        // Level 2 - End
+    };
+});
